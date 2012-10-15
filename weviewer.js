86095113b3991f7c485545c352e5b9d30a76f0cb
Adam Marcus <marcua@marcua.net>
2012-10-14 23:07:30 -0400

    disqus

--- weviewer.js
+++ weviewer.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
       $("#content").append(url)
       var disqus_shortname = 'weviewer'; // required: replace example with your forum shortname
       var disqus_identifier = url;
+      var disqus_title = "This is a test 123";
 
       (function() {
           var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
