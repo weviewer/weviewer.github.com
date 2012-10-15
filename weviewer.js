aa8e79bf5da38abc74a9632ed8ba4bbeefdc7bbf
Adam Marcus <marcua@marcua.net>
2012-10-14 23:00:45 -0400

    disqus

--- weviewer.js
+++ weviewer.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   $.get('data.json', function(data) {
-    $("#content").append(data[window.location.hash].paper_url);
+      var url = data[window.location.hash.slice(1)].paper_url;
+      $("#content").append(url)
+      var disqus_shortname = 'weviewer'; // required: replace example with your forum shortname
+      var disqus_identifier = url;
+
+      (function() {
+          var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+          dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+      })();
   });
 });
